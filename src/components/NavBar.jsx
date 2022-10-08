4db033a5e63229f46d2a263b9097aeee6fb039ea
jamalxwm <jamal@letswayfare.com>
2022-10-08 11:54:13 +0100

    Add VideoHeader

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/projectifylogo.svg'
+
+function NavBar() {
+  return (
+    <div>
+        <img src={logo} alt="projectify logo"></img>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/VideoHeader.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import video_mobile from '../assets/WEBLOOP_mobile.mp4';
+import video_desktop from '../assets/WEBLOOP_desktop.mp4';
+import Media from 'react-media';
+
+export default function VideoHeader() {
+ 
+  return (
+    <div>
+      <Media queries={{
+        mobile: "(max-width: 479px)",
+        desktop: "(min-width: 480px)"
+      }}>
+        {matches => (
+          <div style={{flex: 1}}>
+          {matches.mobile && <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          src={video_mobile}
+          width='100%'
+          type="video/mp4"
+        />}
+          {matches.desktop && <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          src={video_desktop}
+          width='100%'
+          type="video/mp4"
+        />}
+          </div>
+        )}
+      </Media>
+      
+    </div>
+  );
+}
+
+
